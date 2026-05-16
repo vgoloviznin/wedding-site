2be039bc06b4f6513fed1daffccd8d1b2ab03b7c
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2026-05-16 15:52:06 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// window.addEventListener("scroll", () => {
+//   const heroImage = document.querySelector(".hero__image");
+
+//   const offset = window.scrollY * 0.15;
+
+//   heroImage.style.transform = `scale(1.03) translateY(${offset}px)`;
+// });
